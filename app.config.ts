c0e165b4c32112bc67556694b411fda58705430e
Raymond <akdfhr2@gmail.com>
2024-08-16 03:21:39 +0900

    feat: android configuration

---
+++ app.config.ts
@@ -0,0 +1,12 @@
+import { ConfigContext } from "@expo/config";
+
+export default ({ config }: ConfigContext): ConfigContext["config"] => ({
+  ...config,
+  ios: {
+    ...config.ios,
+    config: {
+      ...(config?.ios?.config ?? {}),
+      usesNonExemptEncryption: false,
+    },
+  },
+});
